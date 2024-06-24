9bc9b573fac8550fed60543205b7f79357d2786c
nfmmendes <nilson.fmmendes@outlook.com>
2024-06-24 12:08:02 +0200

    Add example5.cpp

---
+++ example5.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::ostream;
+using std::string;
+
+template <class T = string>
+class Node
+{
+
+public:
+    explicit Node(const T &nodeValue) : value(nodeValue){};
+
+    const T &getValue()
+    {
+        return value;
+    }
+
+    friend ostream &operator<<(ostream &os, const Node<T> &node)
+    {
+        os << " value: " << node.value;
+
+        return os;
+    }
+
+private:
+    const T value{};
+};
+
+template <class T = string>
+class Tree
+{
+
+public:
+    explicit Tree(Node<T> *rootNode) : root(rootNode != nullptr ? rootNode : new Node<T>(T{}))
+    {
+    }
+
+    explicit Tree(T &rootValue) : Tree(new Node<T>(rootValue))
+    {
+    }
+
+    explicit Tree(T &&rootValue) : Tree(new Node<T>(std::forward<T>(rootValue)))
+    {
+    }
+
+    ~Tree()
+    {
+        delete leftSubtree;
+        delete rightSubtree;
+        delete root;
+    }
+
+    const Node<T> *getRoot() const
+    {
+        return root;
+    }
+
+    void addNode(Node<T> *newNode)
+    {
+        if (newNode == nullptr)
+            return;
+
+        if (newNode->getValue() <= root->getValue())
+        {
+            if (leftSubtree == nullptr)
+            {
+                leftSubtree = new Tree(newNode);
+                leftSubtree->setParent(this);
+            }
+            else
+            {
+                leftSubtree->addNode(newNode);
+            }
+        }
+        else
+        {
+            if (rightSubtree == nullptr)
+            {
+                rightSubtree = new Tree(newNode);
+                rightSubtree->setParent(this);
+            }
+            else
+            {
+                rightSubtree->addNode(newNode);
+            }
+        }
+    }
+
+    void addValue(T &&value)
+    {
+        auto *newNode = new Node(std::forward<T>(value));
+        addNode(newNode);
+    }
+
+    const Tree<T> *getLeftSubtree() const
+    {
+        return leftSubtree;
+    }
+
+    void setLeftSubtree(const Tree<T> *left)
+    {
+        delete leftSubtree;
+        leftSubtree = left;
+        leftSubtree->parentTree = this;
+    }
+
+    const Tree<T> *getRightSubtree() const
+    {
+        return rightSubtree;
+    }
+
+    void setRightSubtree(const Tree<T> *right)
+    {
+        delete rightSubtree;
+        rightSubtree = right;
+        rightSubtree->parentTree = this;
+    }
+
+    void print() const
+    {
+        if (leftSubtree != nullptr)
+        {
+            cout << "GOING LEFT\n";
+            leftSubtree->print();
+        }
+
+        cout << "PRINTING ROOT --> " << *root << endl;
+
+        if (rightSubtree != nullptr)
+        {
+            cout << "GOING RIGHT\n";
+            rightSubtree->print();
+        }
+    }
+
+    const Tree<T> *getParent() const
+    {
+        return parentTree;
+    }
+
+private:
+    Node<T> *root{};
+    Tree *leftSubtree{};
+    const Tree *parentTree{};
+    Tree *rightSubtree{};
+
+    void setParent(const Tree<T> *parent)
+    {
+        parentTree = parent;
+    }
+};
+
+template <class T>
+class TreeIterator
+{
+public:
+    explicit TreeIterator(Tree<T> *initTree) : tree(initTree) {}
+    TreeIterator(TreeIterator &other) : tree(other.tree) {}
+
+    const Tree<T> &operator*() const
+    {
+        return *tree;
+    }
+
+    TreeIterator<T> &operator++()
+    {
+        if (tree->getRightSubtree() == nullptr)
+        {
+            if (tree->getParent() == nullptr || tree->getParent()->getRightSubtree() == tree)
+                tree = nullptr;
+            else
+                tree = tree->getParent();
+        }
+        else
+        {
+            tree = getLeftMostOnRight(tree);
+        }
+
+        return *this;
+    }
+
+    bool operator!=(const Tree<T> *other) const
+    {
+        return tree != other;
+    }
+
+private:
+    const Tree<T> *getLeftMostOnRight(const Tree<T> *currentTree)
+    {
+        return leftMostSubTree(currentTree->getRightSubtree(), currentTree);
+    }
+
+    const Tree<T> *leftMostSubTree(const Tree<T> *currentTree, const Tree<T> *parentTree) const
+    {
+        if (currentTree->getLeftSubtree() != nullptr)
+            return leftMostSubTree(currentTree->getLeftSubtree(), currentTree);
+
+        return currentTree;
+    }
+
+    const Tree<T> *tree{};
+};
+
+int main()
+{
+    Tree<int> *tree = new Tree<int>(5);
+    tree->addValue(10);
+    tree->addValue(4);
+    tree->addValue(8);
+    tree->addValue(15);
+    tree->addValue(2);
+    tree->addValue(12);
+
+    tree->print();
+
+    // (5) --R--> (10) --L--> (8)
+    //                 --R--> (15) --L--> (12)
+    //
+    //     --L--> (4)  --L--> (2)
+
+    cout << "Print with iterators " << endl;
+    for (TreeIterator<int> it{tree}; it != nullptr; ++it)
+    {
+        cout << *((*it).getRoot()) << endl;
+    }
+
+    delete tree;
+
+    return 0;
+}
